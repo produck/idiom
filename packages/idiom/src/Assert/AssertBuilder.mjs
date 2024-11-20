c5e8b9274e24ffcf2979be1fc9b89c9906d13dd4
chaoslee <chaos2lee@qq.com>
2024-11-20 10:52:52 +0800

    @produck/idiom: [FIXED] Assert::AssertBuilder: remove returns.

--- packages/idiom/src/Assert/AssertBuilder.mjs
+++ packages/idiom/src/Assert/AssertBuilder.mjs
@@ -4,15 +4,15 @@ import * as Is from '../Is/index.mjs';
 export const Assert = (validator, _expected) => {
 	return (value, role, expected = _expected) => {
 		if (!Is.Type.String(role)) {
-			return Ow.Invalid('role', 'string');
+			Ow.Invalid('role', 'string');
 		}
 
 		if (!Is.Type.String(expected)) {
-			return Ow.Invalid('expected', 'string');
+			Ow.Invalid('expected', 'string');
 		}
 
 		if (!validator(value)) {
-			return Ow.Invalid(role, expected);
+			Ow.Invalid(role, expected);
 		}
 	};
 };
